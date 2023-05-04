9042003928705c934d4992a731e5584f88a5af8b
Pablo Mendez <mendez4a@gmail.com>
2023-05-04 17:46:55 +0200

    add comments to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,19 +4,41 @@ import {
 } from "../params/index.js";
 import { Architecture } from "../types/index.js";
 
-// Single arch images
+/**
+ * Returns the arch tag for the given architecture
+ * @param arch Architecture in the format <os>/<arch>
+ * @returns Arch tag in the format <os>-<arch>
+ */
 export const getArchTag = (arch: Architecture): string =>
   arch.replace(/\//g, "-");
+
+/**
+ * Returns the image path for the given container name, version and architecture
+ * @param name Container name
+ * @param version Container version
+ * @param arch Container architecture in the format <os>/<arch>
+ * @returns Image path in the format <name>_<version>_<os>-<arch>.txz
+ */
 export const getImagePath = (
   name: string,
   version: string,
   arch: Architecture
 ): string => `${name}_${version}_${getArchTag(arch)}.txz`;
+
+/**
+ * Returns the legacy image path for the given container name and version
+ * @param name Container name
+ * @param version Container version
+ * @returns Legacy image path in the format <name>_<version>.tar.xz
+ */
 export const getLegacyImagePath = (name: string, version: string): string =>
   `${name}_${version}.tar.xz`;
 
 /**
- * Get a unique domain per container, considering multi-service packages
+ * Returns a unique domain per container, considering multi-service packages
+ * @param dnpName DAppNode package name
+ * @param serviceName Service name
+ * @returns Container domain in the format <service-name>.<dappnode-package-name> if service-name is not empty and different from dappnode-package-name, otherwise returns dappnode-package-name
  */
 export const getContainerDomain = ({
   dnpName,
@@ -32,6 +54,13 @@ export const getContainerDomain = ({
   }
 };
 
+/**
+ * Returns the image tag for the given DAppNode package name, service name and version
+ * @param dnpName DAppNode package name
+ * @param serviceName Service name
+ * @param version Container version
+ * @returns Image tag in the format <container-domain>:<version>, where container-domain is the domain obtained with getContainerDomain
+ */
 export const getImageTag = ({
   dnpName,
   serviceName,
@@ -42,6 +71,13 @@ export const getImageTag = ({
   version: string;
 }): string => [getContainerDomain({ dnpName, serviceName }), version].join(":");
 
+/**
+ * Returns the container name for the given DAppNode package name, service name and isCore flag
+ * @param dnpName DAppNode package name
+ * @param serviceName Service name
+ * @param isCore Flag indicating whether the container is a core container or not
+ * @returns Container name in the format <container-prefix>-<container-domain>, where container-prefix is the core or non-core prefix obtained from containerCoreNamePrefix and containerNamePrefix, respectively, and container-domain is the domain obtained with getContainerDomain
+ */
 export const getContainerName = ({
   dnpName,
   serviceName,
